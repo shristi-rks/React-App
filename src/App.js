e9e9372396bb90782ca72be19ad77bc7abe608fb
shristi-rks <shristi.n08@gmail.com>
2023-06-30 14:47:41 +0300

    create components and use react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import MyContainer from './components/MyContainer';
+import About from './components/About';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+    <Routes>
+      <Route path = '/' element={<MyContainer />} />
+      <Route path='/about' element={<About/>} />
+    </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/MyContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyContainer() {
+  return (
+    <div>
+      <h1>My Container</h1>
+    </div>
+  )
+}
+
+export default MyContainer
